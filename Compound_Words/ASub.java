784701b0b1c7da6f994c6d29b84120c5492c8967
Ben <i@auburn.edu>
2018-01-22 19:37:19 -0600

    "test upload"

---
+++ Compound_Words/ASub.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.math.*;
+import java.io.*;
+
+public class ASub {
+
+	public static void main(String[] args) {
+
+		Kattio k = new Kattio(System.in);
+
+
+
+	}	
+
+
+	
+}
---
+++ Compound_Words/ZSub.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.math.*;
+import java.io.*;
+
+
+
+public class ZSub {
+
+	public static void main(String[] args) {
+
+		Kattio k = new Kattio(System.in);
+		System.out.println();
+		
+
+	}	
+
+
+	
+}
